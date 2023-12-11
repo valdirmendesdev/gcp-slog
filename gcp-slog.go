36cb09e9c47d743ad2ce40e24200f8149d3cab79
Valdir Mendes <valdir.mfjesus@hotmail.com>
2023-12-11 20:06:33 -0300

    feat: GCP replacement slog options

---
+++ gcp-slog.go
@@ -0,0 +1,30 @@
+package gcp_slog
+
+import (
+	"log/slog"
+	"strings"
+)
+
+func NewGCPHandler() (*slog.HandlerOptions, *slog.LevelVar) {
+	var programLevel = new(slog.LevelVar)
+	return &slog.HandlerOptions{
+		ReplaceAttr: gcpSlogReplaceAttr,
+		Level:       programLevel,
+	}, programLevel
+}
+
+func gcpSlogReplaceAttr(group []string, a slog.Attr) slog.Attr {
+	if a.Key == "level" {
+		return slog.Attr{Key: "severity", Value: slog.StringValue(strings.ToLower(a.Value.String()))}
+	}
+
+	if a.Key == "time" {
+		return slog.Attr{Key: a.Key, Value: slog.AnyValue(a.Value.Time().UnixMilli())}
+	}
+
+	if a.Key == "msg" {
+		return slog.Attr{Key: "message", Value: a.Value}
+	}
+
+	return slog.Attr{Key: a.Key, Value: a.Value}
+}
